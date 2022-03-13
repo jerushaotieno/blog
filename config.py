d2a26674d9841e7d6c8dd7b6f3a8baa85fbf10d8
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-13 17:35:05 +0300

    add secret key to config.py sile

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     '''
     pass
 
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+
 
 
 class ProdConfig(Config):
@@ -27,3 +29,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development' : DevConfig,
+'production':ProdConfig
+}
