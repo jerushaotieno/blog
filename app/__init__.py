cee52bde4afa4f5b567d45dd1417d77db3e89af6
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-15 04:21:11 +0300

    create asender email

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
@@ -17,6 +18,8 @@ bootstrap = Bootstrap()
 
 db=SQLAlchemy()
 
+mail = Mail()
+
 def create_app(config_name):
     app = Flask(__name__)
     # Creating the app configurations
@@ -27,6 +30,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
@@ -39,3 +43,8 @@ photos = UploadSet('photos',IMAGES)
 
 # configure UploadSet
 configure_uploads(app,photos)
+
+
+
+
+
