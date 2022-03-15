462ad431e068507ac7b66eb378a79805da3f4993
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-15 05:20:20 +0300

    update app forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,56 @@
-from wtforms import SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField, TextAreaField, DateField, SelectField
+from ..models import User
+from wtforms import ValidationError
 
 #Edit profile
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
     submit = SubmitField('Submit')
+
+
+# user subscription
+
+class SubscribedUserForm(FlaskForm):
+    email = StringField('Enter your email address to subscribe to our blog',validators=[DataRequired(),Email()])
+    submit = SubmitField('Subscribe')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('This email is in use')
+
+
+# new blog post 
+
+class BlogForm(FlaskForm):
+    title = StringField('Blog Title ',validators = [DataRequired()])
+    description = TextAreaField('Add your blog post ',validators=[DataRequired()])
+    # postedby = StringField('Posted By: ',validators = [DataRequired()])
+    date = DateField('Posting Date', validators=[DataRequired()])
+    submit = SubmitField('Add Blog')
+
+
+# update blog
+
+class UpdateBlog(FlaskForm):
+    description = TextAreaField('Update your blog', validators=[DataRequired()])
+    submit = SubmitField('Update blog')
+
+
+# delete blog post
+
+class DeleteBlog(FlaskForm):
+    description = TextAreaField('Are you sure you want to delete this blog?', validators=[DataRequired()])
+    submit = SubmitField('Delete blog')
+
+
+# comments
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write your comment here',validators=[DataRequired()])
+    # madeby = StringField('Made By: ',validators = [DataRequired()])
+    dateposted = DateField('Posting Date', validators=[DataRequired()])
+    submit = SubmitField('Add Comment')
+
